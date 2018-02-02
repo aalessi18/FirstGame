9a895986819aa6235c16d2cb39208229b42af3e9
Alex <alex@Alexs-MacBook-Pro.local>
2018-02-02 14:19:59 -0500

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Alex on 2018-02-02.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
